4af75cd440d3f4ac23ba510fa90f0b568b97e6e7
DESKTOP-K8TPNO4\Ernes <ernest.petrovic25@gmail.com>
2022-03-27 17:02:05 +0300

    - Make title readonly, and move it to class scope. - Add test case, that checks if page gets back to Home page. - Add test case, that checks if page after reload is still on the same home page.

--- Vgtu.Test/Vgtu.Test/Chrome/GeneralTests.cs
+++ Vgtu.Test/Vgtu.Test/Chrome/GeneralTests.cs
@@ -1,25 +1,57 @@
 using Xunit;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Threading;
 
 namespace Vgtu.Test.Chrome
 {
     public class GeneralTests
     {
         private readonly string _url = "https://vilniustech.lt/";
+        private readonly string _universityPageUri = "universitetas/9";
+        private readonly string _title = "VILNIUS TECH – Vilniaus Gedimino technikos universitetas";
         private readonly IWebDriver _driver = new ChromeDriver();
 
         [Fact]
         public void LoadApplicationPage()
         {
-            string expectedTitle = "VILNIUS TECH – Vilniaus Gedimino technikos universitetas";
             using (_driver)
             {
                 _driver.Navigate().GoToUrl(_url);
                 
                 Assert.Equal(_url, _driver.Url);
-                Assert.Equal(expectedTitle, _driver.Title);
+                Assert.Equal(_title, _driver.Title);
             }
-        } 
+        }
+        
+        [Fact]
+        public void ReloadApplicationPage()
+        {
+            using (_driver)
+            {
+                _driver.Navigate().GoToUrl(_url);
+
+                _driver.Navigate().Refresh();
+
+                Assert.Equal(_url, _driver.Url);
+                Assert.Equal(_title, _driver.Title);
+            }
+        }
+
+        [Fact]
+        public void BackToApplicationHomePage()
+        {
+            using (_driver)
+            {
+                _driver.Navigate().GoToUrl(_url);
+
+                _driver.Navigate().GoToUrl(_url + _universityPageUri);
+                Thread.Sleep(3000);
+                _driver.Navigate().Back();
+
+                Assert.Equal(_url, _driver.Url);
+                Assert.Equal(_title, _driver.Title);
+            }
+        }
     }
 }
