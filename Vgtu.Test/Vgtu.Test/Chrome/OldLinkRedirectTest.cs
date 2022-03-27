845d656e54111fc4dcf73644a6d213aab62a613a
DESKTOP-K8TPNO4\Ernes <ernest.petrovic25@gmail.com>
2022-03-27 22:33:47 +0300

    Add test case, for checking redirection from old url to a new one.

---
+++ Vgtu.Test/Vgtu.Test/Chrome/OldLinkRedirectTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Vgtu.Test.Chrome
+{
+    public class OldLinkRedirectTest
+    {
+        private readonly string _oldUrl = "https://vgtu.lt";
+        private readonly string _url = "https://vilniustech.lt/";
+        private readonly IWebDriver _driver = new ChromeDriver();
+
+        [Fact]
+        public void RedirectOnOldLink()
+        {
+            using (_driver)
+            {
+                _driver.Navigate().GoToUrl(_oldUrl);
+
+                Assert.Equal(_url, _driver.Url);
+            }
+        }
+    }
+}
