fbbd3cb35b03762f15c4f72a494494d181c5c013
DESKTOP-K8TPNO4\Ernes <ernest.petrovic25@gmail.com>
2022-03-28 23:30:06 +0300

    Add test case for checking image elements that are wrapped in "faculty_news" div for containing image class

---
+++ Vgtu.Test/Vgtu.Test/Chrome/ElementTest.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using Xunit;
+
+namespace Vgtu.Test.Chrome
+{
+    public class ElementTest : BaseArrangement
+    {
+        [Fact]
+        public void ImageElementContainsImageClass_FacultyNewsDiv()
+        {
+            using (_driver)
+            {
+                _driver.Manage().Window.Maximize();
+                _driver.Navigate().GoToUrl(Url);
+                string expectedClass = "image";
+                string wrapperClass = "faculty_news";
+                string tagName = "img";
+
+                var imageWrapper = _driver.FindElement(By.ClassName(wrapperClass));
+                var imageElements = imageWrapper.FindElements(By.TagName(tagName));
+
+
+                foreach(var image in imageElements)
+                {
+                    Assert.Equal(expectedClass, image.GetAttribute("class"));
+                }
+            }
+        }
+    }
+}
