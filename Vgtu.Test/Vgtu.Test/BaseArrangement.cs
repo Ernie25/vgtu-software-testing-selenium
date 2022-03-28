e669e17b97bc8a98ebd81cd7203178a271a1140c
DESKTOP-K8TPNO4\Ernes <ernest.petrovic25@gmail.com>
2022-03-28 23:26:23 +0300

    Add Base Arrangement class storing all base reusable properties and fields.

---
+++ Vgtu.Test/Vgtu.Test/BaseArrangement.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Vgtu.Test
+{
+    public class BaseArrangement
+    {
+        protected string Url { get; set; } = "https://vilniustech.lt/";
+        protected string Title { get; set; } = "VILNIUS TECH – Vilniaus Gedimino technikos universitetas";
+        protected readonly string _oldUrl = "https://vgtu.lt";
+        protected readonly string _instagramUrl = "https://www.instagram.com/vilniustech/";
+        protected readonly IWebDriver _driver = new ChromeDriver();
+        protected readonly string _universityPageUri = "universitetas/9";
+
+    }
+}
