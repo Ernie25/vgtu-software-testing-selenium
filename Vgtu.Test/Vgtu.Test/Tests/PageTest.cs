df2526a5a4ccdbbb60921f4be59fce89f47a3d02
DESKTOP-K8TPNO4\Ernes <ernest.petrovic25@gmail.com>
2022-03-30 00:30:19 +0300

    Add test case for checking navigation panels links.

---
+++ Vgtu.Test/Vgtu.Test/Tests/PageTest.cs
@@ -0,0 +1,58 @@
+﻿using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Threading;
+using Vgtu.Test.Helpers;
+using Xunit;
+
+
+namespace Vgtu.Test.Tests
+{
+    public class PageTest : BaseArrangement
+    {
+        [Theory]
+        [MemberData(nameof(Pages))]
+        public void ReachPage(string linkText, string expectedUri)
+        {
+            using (_driver)
+            {                
+                _driver.Manage().Window.Maximize();
+                _driver.Navigate().GoToUrl(Url);
+
+
+                var navigationMenu = _driver.FindElement(By.ClassName("main_menu"));
+                var navigationButton = navigationMenu.FindElement(By.LinkText(linkText));
+                navigationButton?.Click();
+
+                Assert.Equal(StringUrlBuilder.BuildUrl(Url, expectedUri), _driver.Url);
+            }
+        }
+
+        private static IEnumerable<object[]> Pages()
+        {
+            yield return new object[] {
+                "Universitetas",
+                PageUri.UniversityPageUri
+            };
+            yield return new object[] {
+                "Stojantiesiems",
+                PageUri.ApplicantsPageUri
+            };
+            yield return new object[] {
+                "Mokslui",
+                PageUri.EducationPageUri
+            };
+            yield return new object[] {
+                "Verslui",
+                PageUri.BusinessPageUri
+            };
+            yield return new object[] {
+                "Tarptautiškumas",
+                PageUri.InternationalStudentsPageUri
+            };
+            yield return new object[] {
+                "Fakultetai",
+                PageUri.FacultiesPageUri
+            };
+        }
+    }
+}
