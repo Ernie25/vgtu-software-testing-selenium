3c89387ea42ab09e173658a000d9058205ccda2d
DESKTOP-K8TPNO4\Ernes <ernest.petrovic25@gmail.com>
2022-03-30 00:55:51 +0300

    Add test case for checking events for having Description and Date divs

--- Vgtu.Test/Vgtu.Test/Tests/ElementTest.cs
+++ Vgtu.Test/Vgtu.Test/Tests/ElementTest.cs
@@ -26,5 +26,34 @@ namespace Vgtu.Test.Tests
                 }
             }
         }
+        
+        [Fact]
+        public void EventDivHasDescriptionAndDateDivs()
+        {
+            using (_driver)
+            {
+                _driver.Manage().Window.Maximize();
+                _driver.Navigate().GoToUrl(Url);
+
+                var eventsList = _driver.FindElement(By.Id("events_list"));
+                var events = eventsList.FindElements(By.ClassName("events_item"));
+
+                foreach (var e in events) 
+                {
+                    var dateDiv = e.FindElement(By.ClassName("date"));
+                    var month = e.FindElement(By.ClassName("month"));
+                    var descDiv = e.FindElement(By.ClassName("desc"));
+                    var desc = descDiv.FindElement(By.TagName("a"));
+
+
+                    Assert.NotNull(dateDiv);
+                    Assert.NotNull(month);
+                    Assert.NotNull(descDiv);
+                    Assert.NotNull(desc);
+                }
+
+
+            }
+        }
     }
 }
