aea2ac07f8ddfbf88c33c2572001679537687f0b
DESKTOP-K8TPNO4\Ernes <ernest.petrovic25@gmail.com>
2022-03-29 23:23:07 +0300

    Helper for building string links

---
+++ Vgtu.Test/Vgtu.Test/Helpers/StringUrlBuilder.cs
@@ -0,0 +1,29 @@
+﻿namespace Vgtu.Test.Helpers
+{
+    public static class StringUrlBuilder
+    {
+        public static string BuildUrl(string baseUrl, string uri)
+        {
+            if (baseUrl[baseUrl.Length - 1] != '/')
+                baseUrl += '/';
+            return baseUrl + uri;
+        }
+
+        public static string BuildQueryUrl(string baseUrl, string query, string action)
+        {
+            string url = baseUrl + "?q=";
+            query = FillSpaces(query, '+');
+            return url + query + "&act=" + action;
+        }
+
+        public static string FillSpaces(string str, char filler)
+        {
+            if (!string.IsNullOrEmpty(str))
+            {
+                str.Replace(' ', filler);
+                return str;
+            }
+            return "";
+        }
+    }
+}
