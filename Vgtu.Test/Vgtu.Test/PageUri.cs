a7b3a0cd22e1876f8eb09bffa204faa89de4afaa
DESKTOP-K8TPNO4\Ernes <ernest.petrovic25@gmail.com>
2022-03-31 22:29:45 +0300

    Add footer navigation menu's links.

--- Vgtu.Test/Vgtu.Test/PageUri.cs
+++ Vgtu.Test/Vgtu.Test/PageUri.cs
@@ -9,5 +9,8 @@
         public const string BusinessPageUri = "verslui/13";
         public const string InternationalStudentsPageUri = "tarptautiskumas/14";
         public const string FacultiesPageUri = "fakultetai/15";
+        public const string MainContactsUri = "kontaktai/-pagrindiniai-kontaktai/519";
+        public const string DataSecuriytUri = "universitetas/duomenu-saugumas/296093";
+        public const string AlumniUri = "universitetas/alumni/322269?lang=1";
     }
 }
